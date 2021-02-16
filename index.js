a8eada9760f0cb848fe36dfd540e1ef06ee9f44c
predator <aleksey.kukishev@gmail.com>
2021-02-16 11:43:29 +0200

    ccc

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const token = '1461737782:AAG-YX00fKSSM65n9IyF6z5XZmC754FpgaU';
+const token = "1461737782:AAG-YX00fKSSM65n9IyF6z5XZmC754FpgaU";
+const url = `https://www.president.gov.ua/documents/decrees`;
 
 const bot = new TelegramBot(token, {
-    polling: true
+	polling: true,
 });
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Леха ╮(￣_￣)╭!');
+const keyWords = [
+	"склад",
+	"демобілізація",
+	"демобілізації",
+	"демобілізації",
+	"демобілізацію",
+	"демобілізацією",
+	"демобілізації",
+	"запас",
+	"запасу",
+	"запасу",
+	"запас",
+	"запасом",
+	"запасі",
+	"строкової",
+	"строкову",
+	"призовів",
+	"звільнення",
+];
 
-    if (msg.text && msg.text === "start sending") {
-        // setInterval(() => {
-        //     bot.sendMessage(msg.chat.id, "Леха-Кроха");
-        // }, 1000);
+bot.on("message", (msg) => {
+	const chatId = msg.chat.id;
+	bot.sendMessage(chatId, "Леха ╮(￣_￣)╭!");
 
-    }
+	if (msg.text && msg.text === "start sending") {
+		setInterval(() => {
+			axios.get(url).then((response) => {
+				for (const keyWord of keyWords) {
+					if (response.data.toLowerCase().indexOf(keyWord, 0) >= 0) {
+						console.log("Finds", keyWord);
+					}
+				}
+			});
+		}, 1000);
+	}
 });
